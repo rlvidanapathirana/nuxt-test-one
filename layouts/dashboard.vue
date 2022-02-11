2bccda343f561643912c93a13d53193b6c145348
Lakshan Vidanapathirana <rlvidanapathirana@gmail.com>
2022-02-11 18:16:21 +0530

    dashbord updated

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -13,28 +13,6 @@
 
 <script>
 
-//  var navbarPosition = localStorage.getItem('navbarPosition');
-//             var navbarVertical = document.querySelector('.navbar-vertical');
-//             var navbarTopVertical = document.querySelector('.content .navbar-top');
-//             var navbarTop = document.querySelector('[data-layout] .navbar-top');
-//             var navbarTopCombo = document.querySelector('.content [data-navbar-top="combo"]');
-//             if (navbarPosition === 'top') {
-//               navbarTop.removeAttribute('style');
-//               navbarTopVertical.remove(navbarTopVertical);
-//               navbarVertical.remove(navbarVertical);
-//               navbarTopCombo.remove(navbarTopCombo);
-//             } else if (navbarPosition === 'combo') {
-//               navbarVertical.removeAttribute('style');
-//               navbarTopCombo.removeAttribute('style');
-//               navbarTop.remove(navbarTop);
-//               navbarTopVertical.remove(navbarTopVertical);
-//             } else {
-//               navbarVertical.removeAttribute('style');
-//               navbarTopVertical.removeAttribute('style');
-//               navbarTop.remove(navbarTop);
-//               navbarTopCombo.remove(navbarTopCombo);
-//             }
-
 export default {
 
 }
